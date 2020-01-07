6f0afde2d3f754cc6ac805637471cfc49a7feadf
Takuya Matsuyama <hi@craftz.dog>
2020-01-07 18:21:46 +0900

    fix(gist): use emed provider

--- src/providers/gist.js
+++ src/providers/gist.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 const BASE_URL = 'https://gist.github.com/'
+const EMBED_PROVIDER_URL =
+  'https://inkdrop-embed-provider.netlify.com/gist.html'
 
 export function test(url) {
   return url.startsWith(BASE_URL)
@@ -11,29 +13,17 @@ export default function Gist(props) {
   const { href } = props
   const contentFrame = useRef()
   const [frameId] = useState('gist-' + Math.random())
+  const url = `${EMBED_PROVIDER_URL}?url=${encodeURIComponent(
+    href
+  )}&id=${frameId}`
 
-  useEffect(() => {
-    const { current: frame } = contentFrame
-    if (frame && frame.contentDocument) {
-      const html = `
-        <html><body onload="window.parent.postMessage({message: 'embed:adjust-iframe-size', id: '${frameId}', height: document.body.scrollHeight});">
-        <script type="text/javascript" src="${href}.js"></script>
-        <script>
-        document.addEventListener('click', function (event) {
-          if (event.target.tagName === 'A') {
-            event.preventDefault();
-            window.parent.postMessage({ message: 'embed:open-external', uri: event.target.href })
-          }
-        }, false);
-        </script>
-        </body></html>
-      `
-      frame.contentDocument.writeln(html)
-      frame.contentDocument.close()
-    }
-  }, [href])
   return (
-    <iframe id={frameId} className="embed-frame" ref={contentFrame}></iframe>
+    <iframe
+      id={frameId}
+      className="embed-frame"
+      ref={contentFrame}
+      src={url}
+    ></iframe>
   )
 }
 
