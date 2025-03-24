20dba54ed19f7454665b2c49f1907fe91177a88d
Ryan McQuen <rpcm@linux.com>
2025-03-24 08:50:13 -0700

    Clean up Lichess/Chesscom embed providers.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- src/providers/lichess.js
+++ src/providers/lichess.js
@@ -2,7 +2,14 @@ import * as React from 'react'
 import { useRef, useState } from 'react'
 import { isLocal } from '../utils'
 const BASE_URL = 'https://lichess.org/'
-const EMBED_PROVIDER_URL = 'https://inkdrop-embed-provider.vercel.app/lichess.html'
+const EMBED_PROVIDER_URL = 'https://lichess.org/embed/game/'
+
+/* Example:
+
+<iframe src="https://lichess.org/embed/game/<ID>?theme=auto&bg=auto"
+width=600 height=397 frameborder=0></iframe>
+
+*/
 
 export function test(url) {
   return url.startsWith(BASE_URL)
@@ -12,16 +19,19 @@ export default function Lichess(props) {
   const { href } = props
   const contentFrame = useRef()
   const [frameId] = useState('lichess-' + Math.random())
-  const url = `${EMBED_PROVIDER_URL}?url=${encodeURIComponent(
-    href
-  )}&id=${frameId}&origin=${isLocal ? 0 : 1}`
+  const urlObj = new URL(href)
+  const lichessId = urlObj.pathname.split('/').pop()
+  const url = `${EMBED_PROVIDER_URL}${lichessId}?theme=auto&bg=auto`
 
   return (
     <iframe
+      src={url}
       id={frameId}
       className="embed-frame"
       ref={contentFrame}
-      src={url}
+      width="600"
+      height="397"
+      frameborder="0"
     ></iframe>
   )
 }
