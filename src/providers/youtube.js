9aba946b58b84e63e8ae651ff130753cc6ddeb5d
Takuya Matsuyama <hi@craftz.dog>
2025-07-15 15:26:58 +0900

    feat(youtube): support https://youtu.be/

--- src/providers/youtube.js
+++ src/providers/youtube.js
@@ -1,30 +1,49 @@
 import * as React from 'react'
 import { useRef, useState } from 'react'
-const BASE_URL = 'https://www.youtube.com/'
+const BASE_URLS = ['https://www.youtube.com/', 'https://youtu.be/']
 const EMBED_PROVIDER_URL = 'https://www.youtube.com/embed/'
 
 export function test(url) {
-  return url.startsWith(BASE_URL)
+  return BASE_URLS.some(baseUrl => url.startsWith(baseUrl))
+}
+
+export function extractVideoIdFromURL(url) {
+  if (url.startsWith('https://www.youtube.com/')) {
+    const query = new URLSearchParams(url.split('?')[1])
+    return query.get('v')
+  } else if (url.startsWith('https://youtu.be/')) {
+    const parts = url.split('/')
+    return parts[parts.length - 1]
+  }
+  return null
 }
 
 export default function YouTube(props) {
   const { href } = props
   const contentFrame = useRef()
   const [frameId] = useState('youtube-' + Math.random())
-  const query = new URLSearchParams(href.split('?')[1])
-  const videoId = query.get('v')
-  const url = `${EMBED_PROVIDER_URL}${videoId}`
+  const videoId = extractVideoIdFromURL(href)
 
-  return (
-    <iframe
-      id={frameId}
-      className="embed-frame"
-      ref={contentFrame}
-      height="344"
-      src={url}
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
-  )
+  if (videoId) {
+    const url = `${EMBED_PROVIDER_URL}${videoId}`
+
+    return (
+      <iframe
+        id={frameId}
+        className="embed-frame"
+        ref={contentFrame}
+        height="344"
+        src={url}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    )
+  } else {
+    return (
+      <div className="embed-error">
+        <p>Invalid YouTube URL. Cannot extract a video ID.</p>
+      </div>
+    )
+  }
 }
