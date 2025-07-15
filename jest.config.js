8ce572cd7edd2612623bdabe776a8f13e5b972d0
Takuya Matsuyama <hi@craftz.dog>
2025-07-15 15:35:36 +0900

    ci(github): set up

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
   testMatch: ['**/__tests__/**/*.test.js'],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
 }
